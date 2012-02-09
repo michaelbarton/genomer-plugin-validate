4787e4c157600116f6bbdf3c0ffba7b9465498ed
Michael Barton <mail@michaelbarton.me.uk>
2012-02-09 10:33:38 -0500

    Initial project skeleton

---
+++ lib/genomer-plugin-validate.rb
@@ -0,0 +1,9 @@
+require "genomer-plugin-validate/version"
+
+module Genomer
+  module Plugin
+    module Validate
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/genomer-plugin-validate/version.rb
@@ -0,0 +1,7 @@
+module Genomer
+  module Plugin
+    module Validate
+      VERSION = "0.0.1"
+    end
+  end
+end
