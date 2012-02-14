d6602534e50198498646bc46904c319e523f94d8
Michael Barton <mail@michaelbarton.me.uk>
2012-02-14 13:31:48 -0500

    Refactor out organise by method

--- lib/genomer-plugin-validate/annotations.rb
+++ lib/genomer-plugin-validate/annotations.rb
@@ -12,21 +12,21 @@ class GenomerPluginValidate::Annotations < Genomer::Plugin
     end.flatten.uniq * "\n" + "\n"
   end
 
-  def validate_for_duplicate_ids(annotations)
-    ids = annotations.inject(Hash.new{|h,k| h[k] = []}) do |hash,attn|
-      hash[attn.id] <<= attn
-      hash
-    end
-    ids.values.select{|v| v.length > 1}.flatten
+  def validate_for_duplicate_ids(attns)
+    organise_by(attns,&:id).values.select{|v| v.length > 1}.flatten
   end
 
-  def validate_for_identical_locations(annotations)
-    locs = annotations.inject(Hash.new{|h,k| h[k] = []}) do |hash,attn|
-      hash[[attn.start..attn.end]] <<= attn
+  def validate_for_identical_locations(attns)
+    organise_by(attns){|i| [i.start,i.end]}.values.select{|v| v.length > 1}
+  end
+
+  private
+
+  def organise_by(annotations,&block)
+    annotations.inject(Hash.new{|h,k| h[k] = []}) do |hash,attn|
+      hash[yield(attn)] <<= attn
       hash
     end
-
-    locs.values.select{|v| v.length > 1}
   end
 
 end
